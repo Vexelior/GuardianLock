5e783c83c0ac3be13746662717cc87d80ffbed9c
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2024-01-24 15:06:34 -0800

    home view and model

---
+++ GuardianLock/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GuardianLock.Core;
+using System;
+
+namespace GuardianLock.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
--- GuardianLock/obj/Debug/net8.0-windows/GuardianLock.AssemblyInfo.cs
+++ GuardianLock/obj/Debug/net8.0-windows/GuardianLock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuardianLock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd8657b7ac4d16d388cdc100cb15343f702f3128")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2afb173c44abcc3b4723a3e34a4749410e498c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuardianLock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuardianLock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
