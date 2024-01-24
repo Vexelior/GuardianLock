d6d7fe104305114ca8e0bc6dde1f471c00c69d66
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2024-01-24 10:58:40 -0800

    create observable objects

---
+++ GuardianLock/Core/ObservableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GuardianLock.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises the PropertyChanged event for the specified property.
+        /// </summary>
+        /// <param name="name">The name of the property that changed.</param>
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ GuardianLock/Core/RelayCommand.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Input;
+
+namespace GuardianLock.Core
+{
+    /// <summary>
+    /// Represents a command that can be executed and provides a mechanism for determining if it can be executed.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _Execute;
+        private Func<object, bool> _CanExecute;
+
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command can execute.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the RelayCommand class.
+        /// </summary>
+        /// <param name="execute">The action to execute when the command is executed.</param>
+        /// <param name="canExecute">The function that determines if the command can be executed.</param>
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return _CanExecute == null || _CanExecute(parameter);
+        }
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.</param>
+        public void Execute(object parameter)
+        {
+            _Execute?.Invoke(parameter);
+        }
+    }
+}
