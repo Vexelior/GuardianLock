a13f14af585ba20c0375addeb1358a5d7ab9985e
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2024-01-24 15:24:33 -0800

    temp

--- GuardianLock/obj/Debug/net8.0-windows/GuardianLock.AssemblyInfo.cs
+++ GuardianLock/obj/Debug/net8.0-windows/GuardianLock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuardianLock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd176d93955f5220abad4efbfa817d606834830a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuardianLock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuardianLock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
