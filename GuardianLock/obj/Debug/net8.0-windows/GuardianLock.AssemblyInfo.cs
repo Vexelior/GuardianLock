bd8657b7ac4d16d388cdc100cb15343f702f3128
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2024-01-24 13:53:46 -0800

    menu button theme

--- GuardianLock/obj/Debug/net8.0-windows/GuardianLock.AssemblyInfo.cs
+++ GuardianLock/obj/Debug/net8.0-windows/GuardianLock.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuardianLock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b74d912a57930c8ec61dfd614f17bd3dd0a053")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d7fe104305114ca8e0bc6dde1f471c00c69d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuardianLock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuardianLock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
